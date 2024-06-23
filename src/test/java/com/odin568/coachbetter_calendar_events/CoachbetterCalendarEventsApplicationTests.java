e63d4116a016443eb8278cd4b353b7ceafcda989
odin568 <markus@zellner-mail.de>
2024-06-23 21:21:10 +0200

    Initializr

---
+++ src/test/java/com/odin568/coachbetter_calendar_events/CoachbetterCalendarEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odin568.coachbetter_calendar_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachbetterCalendarEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
