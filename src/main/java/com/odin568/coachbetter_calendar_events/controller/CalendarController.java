a23e071323711416b919b9fa687907d6e8e403ad
odin568 <markus@zellner-mail.de>
2024-06-24 15:09:30 +0200

    Add simple authentication

--- src/main/java/com/odin568/coachbetter_calendar_events/controller/CalendarController.java
+++ src/main/java/com/odin568/coachbetter_calendar_events/controller/CalendarController.java
@@ -4,12 +4,17 @@ import com.odin568.coachbetter_calendar_events.service.CalendarService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("calendar")
@@ -17,19 +22,27 @@ public class CalendarController {
 
     private final Logger logger = LoggerFactory.getLogger(CalendarController.class);
     private final CalendarService calendarService;
+    private final Optional<String> configuredSecret;
 
     @Autowired
-    public CalendarController(CalendarService calendarService)
+    public CalendarController(CalendarService calendarService, @Value("${secret:#{null}}") Optional<String> secret)
     {
         this.calendarService = calendarService;
+        this.configuredSecret = secret;
     }
 
     @GetMapping(value = "/team", produces = "text/calendar")
-    public ResponseEntity<String> getTeamCalendar()
+    public ResponseEntity<String> getTeamCalendar(@RequestParam(value = "secret", required = false) Optional<String> currentSecret)
     {
-        logger.info("GET /team");
+        if (configuredSecret.isPresent()){
+            if (currentSecret.isEmpty() || !configuredSecret.get().equals(currentSecret.get()))
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        logger.info("GET /team - START");
         HttpHeaders header = new HttpHeaders();
         header.add("content-disposition", "attachment; filename=\"team.ics\"");
+        logger.info("GET /team - END");
         return new ResponseEntity<>(calendarService.GetTeamCalendar(), header, HttpStatus.OK);
     }
 }
